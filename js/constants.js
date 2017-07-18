ca749cf0dae5ce76a3569f4bccf0c1302cbcb248
Nithin Baby <nithin.baby@cognizant.com>
2017-07-19 02:00:53 +0530

    path

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
 app.constant('AppConstants', {
-
+  DUMMY_IMAGE: "placeholder_for_missing_posters.png",
   API_LOCATION: "https://nithinbaby.github.io/videoapp/API/CONTENTLISTINGPAGE-PAGE",
-  ROOT_FOLDER: "https://github.com/nithinbaby/videoapp/"
+  ROOT_FOLDER: "https://nithinbaby.github.io/videoapp/",
 });
