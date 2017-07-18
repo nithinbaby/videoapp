e483358695ae7f5deb32db02e98b6d229d10268c
Nithin Baby <nithin.baby@cognizant.com>
2017-07-19 01:44:38 +0530

    web service path changed

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,4 @@
 app.constant('AppConstants', {
-  API_LOCATION: "../API/CONTENTLISTINGPAGE-PAGE",
+  API_LOCATION: "./API/CONTENTLISTINGPAGE-PAGE",
   DUMMY_IMAGE: "placeholder_for_missing_posters.png"
 });
