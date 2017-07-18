8f509394d417de5b4a0e34c41f2cd7ed3718115c
nithinbaby <nithinb24@gmail.com>
2017-07-19 01:50:19 +0530

    full path added

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,4 @@
 app.constant('AppConstants', {
-  API_LOCATION: "./videoapp/API/CONTENTLISTINGPAGE-PAGE",
+  API_LOCATION: "https://nithinbaby.github.io/videoapp/API/CONTENTLISTINGPAGE-PAGE",
   DUMMY_IMAGE: "placeholder_for_missing_posters.png"
 });
